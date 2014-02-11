b0914f89a0bf4fa77947077a2f8513af020fe987
cpoteet <cpoteet@siolon.com>
2014-02-11 13:22:03 -0500

    Added browser/touch information script

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ jQuery(document).ready(function($) {
     "use strict";
     
 });
+
+// Add browser/touch information
+
+var b = document.documentElement;
+b.setAttribute('data-useragent',  navigator.userAgent);
+b.setAttribute('data-platform', navigator.platform );
+b.className += ((!!('ontouchstart' in window) || !!('onmsgesturechange' in window))?' touch':'');
